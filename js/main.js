d84ffad5bd993feee4dca209a36f172dd602d9cc
James <james@wallofhistory.com>
2022-08-10 23:08:08 -0500

    Fix typos

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function download(url) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = url.split('/').pop();
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
